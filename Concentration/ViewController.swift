78a4ed9e8aaa944eae5cf6f980e4ec66a99df380
Vitaly Khryapin <vitkhryapin@gmail.com>
2021-11-17 10:17:03 +0300

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Vitaly Khryapin on 17.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
